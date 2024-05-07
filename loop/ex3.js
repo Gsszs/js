6771007ca9bafc21b9abdc2f5c71217caf5f4143
Gsszs <gabriel.selvenca@icloud.com>
2024-05-07 16:43:52 -0300

    feat: Exercicio 2/loop completo

---
+++ loop/ex3.js
@@ -0,0 +1,7 @@
+// 3. Faça um programa que imprima os números ímpares de 0 a 50
+
+for (contador = 0; contador <=50; contador++) {
+    if (contador%2 != 0) {
+        console.log(contador)
+    }
+}
